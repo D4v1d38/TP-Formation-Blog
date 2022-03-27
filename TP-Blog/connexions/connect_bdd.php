2dedf3ae7c237b20ab35c5baafc163b5e1f02571
David <89340222+D4v1d38@users.noreply.github.com>
2022-03-27 17:03:50 +0200

    Update connect_bdd.php

--- TP-Blog/connexions/connect_bdd.php
+++ TP-Blog/connexions/connect_bdd.php
@@ -3,7 +3,7 @@
 //connexion à la base de données
 try{
     
-    $bdd = new PDO('mysql:host=db.3wa.io;dbname=davidrotolo_blog;charset=utf8','davidrotolo','1dab2f9c1a3dc3f96a1229b7f7684115');
+    $bdd = new PDO('mysql:host=***host***;dbname=davidrotolo_blog;charset=utf8','***username***','***password***');
 
     
 }
